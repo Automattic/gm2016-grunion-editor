8e0a66f54f903d970c302612248d3dd3a7204375
George Stephanis <georgestephanis@automattic.com>
2016-09-16 11:26:13 -0700

    Add JS to allow playing with the data on edit.

--- js/editor-view.js
+++ js/editor-view.js
@@ -19,10 +19,22 @@
             return this.template( options );
         },
         edit: function( data ) {
-            var shortcode_data    = wp.shortcode.next( this.shortcode_string, data),
-                named             = shortcode_data.shortcode.attrs.named,
-                editor            = tinyMCE.activeEditor,
-                renderer          = this;
+            var shortcode_data = wp.shortcode.next( this.shortcode_string, data ),
+                named          = shortcode_data.shortcode.attrs.named,
+                content        = shortcode_data.shortcode.content,
+                editor         = tinyMCE.activeEditor,
+                renderer       = this,
+                args           = {
+                    tag     : 'contact-form',
+                    type    : 'closed',
+                    content : content,
+                    // Only keep keys we recognize.
+                    attrs   : _.pick( named, _.keys( renderer.defaults ) )
+                };
+
+            // ACTUALLY DO SOMETHING TO PROMPT THE USER TO CHANGE THIS STUFF BEFORE RE-ENTERING IT.
+
+            editor.insertContent( wp.shortcode.string( args ) );
         }
     };
     wp.mce.views.register( 'contact-form', wp.mce.grunion_wp_view_renderer );
