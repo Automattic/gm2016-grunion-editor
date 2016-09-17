fa2d843d031ad3107f0ad5efa69dd23d05b06c51
George Stephanis <georgestephanis@automattic.com>
2016-09-17 16:11:51 -0700

    Spaces to tabs!

--- grunion-editor.php
+++ grunion-editor.php
@@ -10,201 +10,201 @@
  */
 
 class Grunion_Editor_View {
-    public static function add_hooks() {
-        add_action( 'admin_notices', array( __CLASS__, 'handle_editor_view_js' ) );
-    }
+	public static function add_hooks() {
+		add_action( 'admin_notices', array( __CLASS__, 'handle_editor_view_js' ) );
+	}
 
-    /**
-     * WordPress Shortcode Editor View JS Code
-     */
-    public static function handle_editor_view_js() {
-        $current_screen = get_current_screen();
-        if ( ! isset( $current_screen->id ) || $current_screen->base !== 'post' ) {
-            return;
-        }
+	/**
+	 * WordPress Shortcode Editor View JS Code
+	 */
+	public static function handle_editor_view_js() {
+		$current_screen = get_current_screen();
+		if ( ! isset( $current_screen->id ) || $current_screen->base !== 'post' ) {
+			return;
+		}
 
-        add_action( 'admin_print_footer_scripts', array( __CLASS__, 'editor_view_js_templates' ) );
+		add_action( 'admin_print_footer_scripts', array( __CLASS__, 'editor_view_js_templates' ) );
 
-        wp_enqueue_style( 'grunion-editor-ui', plugins_url( 'css/editor-ui.css', __FILE__ ) );
-        wp_enqueue_script( 'grunion-editor-view', plugins_url( 'js/editor-view.js', __FILE__ ), array( 'wp-util', 'jquery' ), false, true );
-        wp_localize_script( 'grunion-editor-view', 'grunionEditorView', array(
-            'home_url_host'     => parse_url( home_url(), PHP_URL_HOST ),
-            'default_form'  => '[contact-field label="' . __( 'Name', 'jetpack' ) . '" type="name"  required="true" /]
-                                [contact-field label="' . __( 'Email', 'jetpack' )   . '" type="email" required="true" /]
-                                [contact-field label="' . __( 'Website', 'jetpack' ) . '" type="url" /]
-                                [contact-field label="' . __( 'Message', 'jetpack' ) . '" type="textarea" /]',
-            'labels'      => array(
-                'submit_button_text' => __( 'Submit', 'jetpack' ),
-                'required_field_text' => apply_filters( 'jetpack_required_field_text', __( '(required)', 'jetpack' ) ),
-                'edit_close_ays' => __( 'Are you sure you\'d like to stop editing this form without saving your changes?', 'jetpack' ),
-            )
-        ) );
+		wp_enqueue_style( 'grunion-editor-ui', plugins_url( 'css/editor-ui.css', __FILE__ ) );
+		wp_enqueue_script( 'grunion-editor-view', plugins_url( 'js/editor-view.js', __FILE__ ), array( 'wp-util', 'jquery' ), false, true );
+		wp_localize_script( 'grunion-editor-view', 'grunionEditorView', array(
+			'home_url_host'     => parse_url( home_url(), PHP_URL_HOST ),
+			'default_form'  => '[contact-field label="' . __( 'Name', 'jetpack' ) . '" type="name"  required="true" /]
+								[contact-field label="' . __( 'Email', 'jetpack' )   . '" type="email" required="true" /]
+								[contact-field label="' . __( 'Website', 'jetpack' ) . '" type="url" /]
+								[contact-field label="' . __( 'Message', 'jetpack' ) . '" type="textarea" /]',
+			'labels'      => array(
+				'submit_button_text' => __( 'Submit', 'jetpack' ),
+				'required_field_text' => apply_filters( 'jetpack_required_field_text', __( '(required)', 'jetpack' ) ),
+				'edit_close_ays' => __( 'Are you sure you\'d like to stop editing this form without saving your changes?', 'jetpack' ),
+			)
+		) );
 
-        add_editor_style( plugins_url( 'css/editor-style.css', __FILE__ ) );
-    }
+		add_editor_style( plugins_url( 'css/editor-style.css', __FILE__ ) );
+	}
 
-    /**
-     * JS Templates.
-     */
-    public static function editor_view_js_templates() {
-        ?>
+	/**
+	 * JS Templates.
+	 */
+	public static function editor_view_js_templates() {
+		?>
 <script type="text/html" id="tmpl-grunion-contact-form">
-    <form class="card" action='#' method='post' class='contact-form commentsblock' onsubmit="return false;">
-        {{{ data.body }}}
-        <p class='contact-submit'>
-            <input type='submit' value='{{ data.submit_button_text }}' class='pushbutton-wide'/>
-        </p>
-    </form>
+	<form class="card" action='#' method='post' class='contact-form commentsblock' onsubmit="return false;">
+		{{{ data.body }}}
+		<p class='contact-submit'>
+			<input type='submit' value='{{ data.submit_button_text }}' class='pushbutton-wide'/>
+		</p>
+	</form>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-email">
-    <div>
-        <label for='{{ data.id }}' class='grunion-field-label email'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <input type='email' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
-    </div>
+	<div>
+		<label for='{{ data.id }}' class='grunion-field-label email'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<input type='email' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-telephone">
-    <div>
-        <label for='{{ data.id }}' class='grunion-field-label telephone'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <input type='tel' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
-    </div>
+	<div>
+		<label for='{{ data.id }}' class='grunion-field-label telephone'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<input type='tel' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-textarea">
-    <div>
-        <label for='contact-form-comment-{{ data.id }}' class='grunion-field-label textarea'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <textarea name='{{ data.id }}' id='contact-form-comment-{{ data.id }}' rows='20' class='{{ data.class }}' placeholder='{{ data.placeholder }}'>{{ data.value }}</textarea>
-    </div>
+	<div>
+		<label for='contact-form-comment-{{ data.id }}' class='grunion-field-label textarea'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<textarea name='{{ data.id }}' id='contact-form-comment-{{ data.id }}' rows='20' class='{{ data.class }}' placeholder='{{ data.placeholder }}'>{{ data.value }}</textarea>
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-radio">
-    <div>
-        <label class='grunion-field-label'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <# _.each( data.options, function( option ) { #>
-            <label class='grunion-radio-label radio'>
-                <input type='radio' name='{{ data.id }}' value='{{ option }}' class="{{ data.class }}" <# if ( option === data.value ) print( "checked='checked'" ) #> />
-                <span>{{ option }}</span>
-            </label>
-        <# }); #>
-        <div class='clear-form'></div>
-    </div>
+	<div>
+		<label class='grunion-field-label'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<# _.each( data.options, function( option ) { #>
+			<label class='grunion-radio-label radio'>
+				<input type='radio' name='{{ data.id }}' value='{{ option }}' class="{{ data.class }}" <# if ( option === data.value ) print( "checked='checked'" ) #> />
+				<span>{{ option }}</span>
+			</label>
+		<# }); #>
+		<div class='clear-form'></div>
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-checkbox">
-    <div>
-        <label class='grunion-field-label checkbox'>
-            <input type='checkbox' name='{{ data.id }}' value='<?php esc_attr__( 'Yes', 'jetpack' ); ?>' class="{{ data.class }}" <# if ( data.value ) print( 'checked="checked"' ) #> />
-                <span>{{ data.label }}</span><# if ( data.required ) print( " <span>" + data.required + "</span>" ) #>
-        </label>
-        <div class='clear-form'></div>
-    </div>
+	<div>
+		<label class='grunion-field-label checkbox'>
+			<input type='checkbox' name='{{ data.id }}' value='<?php esc_attr__( 'Yes', 'jetpack' ); ?>' class="{{ data.class }}" <# if ( data.value ) print( 'checked="checked"' ) #> />
+				<span>{{ data.label }}</span><# if ( data.required ) print( " <span>" + data.required + "</span>" ) #>
+		</label>
+		<div class='clear-form'></div>
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-checkbox-multiple">
-    <div>
-        <label class='grunion-field-label'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <# _.each( data.options, function( option ) { #>
-            <label class='grunion-checkbox-multiple-label checkbox-multiple'>
-                <input type='checkbox' name='{{ data.id }}[]' value='{{ option }}' class="{{ data.class }}" <# if ( option === data.value || _.contains( data.value, option ) ) print( "checked='checked'" ) #> />
-                <span>{{ option }}</span>
-            </label>
-        <# }); #>
-        <div class='clear-form'></div>
-    </div>
+	<div>
+		<label class='grunion-field-label'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<# _.each( data.options, function( option ) { #>
+			<label class='grunion-checkbox-multiple-label checkbox-multiple'>
+				<input type='checkbox' name='{{ data.id }}[]' value='{{ option }}' class="{{ data.class }}" <# if ( option === data.value || _.contains( data.value, option ) ) print( "checked='checked'" ) #> />
+				<span>{{ option }}</span>
+			</label>
+		<# }); #>
+		<div class='clear-form'></div>
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-select">
-    <div>
-        <label for='{{ data.id }}' class='grunion-field-label select'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <select name='{{ data.id }}' id='{{ data.id }}' class="{{ data.class }}">
-            <# _.each( data.options, function( option ) { #>
-                <option <# if ( option === data.value ) print( "selected='selected'" ) #>>foo{{ option }}</option>
-            <# }); #>
-        </select>
-    </div>
+	<div>
+		<label for='{{ data.id }}' class='grunion-field-label select'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<select name='{{ data.id }}' id='{{ data.id }}' class="{{ data.class }}">
+			<# _.each( data.options, function( option ) { #>
+				<option <# if ( option === data.value ) print( "selected='selected'" ) #>>foo{{ option }}</option>
+			<# }); #>
+		</select>
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-date">
-    <div>
-        <label for='{{ data.id }}' class='grunion-field-label {{ data.type }}'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <input type='date' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class="{{ data.class }}" />
-    </div>
+	<div>
+		<label for='{{ data.id }}' class='grunion-field-label {{ data.type }}'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<input type='date' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class="{{ data.class }}" />
+	</div>
 </script>
 
 <script type="text/html" id="tmpl-grunion-field-text">
-    <div>
-        <label for='{{ data.id }}' class='grunion-field-label {{ data.type }}'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
-        <input type='text' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
-    </div>
+	<div>
+		<label for='{{ data.id }}' class='grunion-field-label {{ data.type }}'>{{ data.label }}<# if ( data.required ) print( " <span>" + data.required + "</span>" ) #></label>
+		<input type='text' name='{{ data.id }}' id='{{ data.id }}' value='{{ data.value }}' class='{{ data.class }}' placeholder='{{ data.placeholder }}' />
+	</div>
 </script>
 
 <!-- Just dummy stuff for now. -->
 <script type="text/html" id="tmpl-grunion-field-edit">
-    <div class="grunion-field-edit grunion-field-{{ data.type }}">
-        <input type="text" name="label" placeholder="<?php esc_attr_e( 'Label', 'jetpack' ); ?>" value="{{ data.label }}"/>
+	<div class="grunion-field-edit grunion-field-{{ data.type }}">
+		<input type="text" name="label" placeholder="<?php esc_attr_e( 'Label', 'jetpack' ); ?>" value="{{ data.label }}"/>
 
-        <?php
-        $grunion_field_types = array(
-            'text'              => __( 'Text', 'jetpack' ),
-            'name'              => __( 'Name', 'jetpack' ),
-            'email'             => __( 'Email', 'jetpack' ),
-            'url'               => __( 'Website', 'jetpack' ),
-            'textarea'          => __( 'Textarea', 'jetpack' ),
-            'checkbox'          => __( 'Checkbox', 'jetpack' ),
-            'checkbox-multiple' => __( 'Checkbox with Multiple Items', 'jetpack' ),
-            'select'            => __( 'Drop down', 'jetpack' ),
-            'radio'             => __( 'Radio', 'jetpack' ),
-        );
-        ?>
-        <label>
-            <?php esc_html_e( 'Field Type', 'jetpack' ); ?>
-            <select name="type">
-                <?php foreach ( $grunion_field_types as $type => $label ) : ?>
-                <option <# if ( '<?php echo esc_js( $type ); ?>' === data.type ) print( "selected='selected'" ) #> value="<?php echo esc_attr( $type ); ?>">
-                    <?php echo esc_html( $label ); ?>
-                    </option>
-                    <?php endforeach; ?>
-            </select>
-        </label>
+		<?php
+		$grunion_field_types = array(
+			'text'              => __( 'Text', 'jetpack' ),
+			'name'              => __( 'Name', 'jetpack' ),
+			'email'             => __( 'Email', 'jetpack' ),
+			'url'               => __( 'Website', 'jetpack' ),
+			'textarea'          => __( 'Textarea', 'jetpack' ),
+			'checkbox'          => __( 'Checkbox', 'jetpack' ),
+			'checkbox-multiple' => __( 'Checkbox with Multiple Items', 'jetpack' ),
+			'select'            => __( 'Drop down', 'jetpack' ),
+			'radio'             => __( 'Radio', 'jetpack' ),
+		);
+		?>
+		<label>
+			<?php esc_html_e( 'Field Type', 'jetpack' ); ?>
+			<select name="type">
+				<?php foreach ( $grunion_field_types as $type => $label ) : ?>
+				<option <# if ( '<?php echo esc_js( $type ); ?>' === data.type ) print( "selected='selected'" ) #> value="<?php echo esc_attr( $type ); ?>">
+					<?php echo esc_html( $label ); ?>
+					</option>
+					<?php endforeach; ?>
+			</select>
+		</label>
 
-        <label>
-            <input type="checkbox" name="required" value="1" <# if ( data.required ) print( 'checked="checked"' ) #> />
-            <?php esc_html_e( 'Required?', 'jetpack' ); ?>
-        </label>
+		<label>
+			<input type="checkbox" name="required" value="1" <# if ( data.required ) print( 'checked="checked"' ) #> />
+			<?php esc_html_e( 'Required?', 'jetpack' ); ?>
+		</label>
 
-        <label class="options">
-            <?php esc_html_e( 'Options', 'jetpack' ); ?>
-            <ol>
-                <# if ( data.options ) { #>
-                    <# _.each( data.options, function( option ) { #>
-                        <li><input type="text" name="option" value="{{ option }}" /> <a href="#">&times;</a></li>
-                    <# }); #>
-                <# } else { #>
-                    <li><input type="text" name="option" /> <a href="#">&times;</a></li>
-                    <li><input type="text" name="option" /> <a href="#">&times;</a></li>
-                    <li><input type="text" name="option" /> <a href="#">&times;</a></li>
-                <# } #>
-                <li><a href="#"><?php esc_html_e( 'Add new option...', 'jetpack' ); ?></a></li>
-            </ol>
-        </label>
-    </div>
+		<label class="options">
+			<?php esc_html_e( 'Options', 'jetpack' ); ?>
+			<ol>
+				<# if ( data.options ) { #>
+					<# _.each( data.options, function( option ) { #>
+						<li><input type="text" name="option" value="{{ option }}" /> <a href="#">&times;</a></li>
+					<# }); #>
+				<# } else { #>
+					<li><input type="text" name="option" /> <a href="#">&times;</a></li>
+					<li><input type="text" name="option" /> <a href="#">&times;</a></li>
+					<li><input type="text" name="option" /> <a href="#">&times;</a></li>
+				<# } #>
+				<li><a href="#"><?php esc_html_e( 'Add new option...', 'jetpack' ); ?></a></li>
+			</ol>
+		</label>
+	</div>
 </script>
 
 <div id="grunion-modal-wrap" style="position:relative;display:none">
-    <div class="grunion-modal wp-core-ui">
-        <div class="fields">
+	<div class="grunion-modal wp-core-ui">
+		<div class="fields">
 
-        </div>
-        <div class="form-settings">
+		</div>
+		<div class="form-settings">
 
-        </div>
-        <?php submit_button( esc_html__( 'Update Form', 'jetpack' ), 'primary' ); ?>
-    </div>
-    <div class="grunion-modal-backdrop"></div>
+		</div>
+		<?php submit_button( esc_html__( 'Update Form', 'jetpack' ), 'primary' ); ?>
+	</div>
+	<div class="grunion-modal-backdrop"></div>
 </div>
-    <?php
-    }
+	<?php
+	}
 }
 
 
--- js/editor-view.js
+++ js/editor-view.js
@@ -1,151 +1,151 @@
 /* global grunionEditorView */
 (function( $, wp, grunionEditorView ){
-    wp.mce = wp.mce || {};
-    if ( 'undefined' === typeof wp.mce.views ) {
-        return;
-    }
+	wp.mce = wp.mce || {};
+	if ( 'undefined' === typeof wp.mce.views ) {
+		return;
+	}
 
-    var $modal_wrap, open_modal, save_close, prompt_close;
+	var $modal_wrap, open_modal, save_close, prompt_close;
 
-    wp.mce.grunion_wp_view_renderer = {
-        shortcode_string : 'contact-form',
-        shortcode_data : {},
-        defaults       : {
-            to      : '',
-            subject : ''
-        },
-        coerce         : wp.media.coerce,
-        template       : wp.template( 'grunion-contact-form' ),
-        edit_template  : wp.template( 'grunion-field-edit' ),
-        field_templates: {
-            email               : wp.template( 'grunion-field-email' ),
-            telephone           : wp.template( 'grunion-field-telephone' ),
-            textarea            : wp.template( 'grunion-field-textarea' ),
-            radio               : wp.template( 'grunion-field-radio' ),
-            checkbox            : wp.template( 'grunion-field-checkbox' ),
-            "checkbox-multiple" : wp.template( 'grunion-field-checkbox-multiple' ),
-            select              : wp.template( 'grunion-field-select' ),
-            date                : wp.template( 'grunion-field-date' ),
-            text                : wp.template( 'grunion-field-text' )
-        },
-        getContent     : function() {
-            var content = this.shortcode.content,
-                index = 0,
-                field, named,
-                body = '';
+	wp.mce.grunion_wp_view_renderer = {
+		shortcode_string : 'contact-form',
+		shortcode_data : {},
+		defaults       : {
+			to      : '',
+			subject : ''
+		},
+		coerce         : wp.media.coerce,
+		template       : wp.template( 'grunion-contact-form' ),
+		edit_template  : wp.template( 'grunion-field-edit' ),
+		field_templates: {
+			email               : wp.template( 'grunion-field-email' ),
+			telephone           : wp.template( 'grunion-field-telephone' ),
+			textarea            : wp.template( 'grunion-field-textarea' ),
+			radio               : wp.template( 'grunion-field-radio' ),
+			checkbox            : wp.template( 'grunion-field-checkbox' ),
+			"checkbox-multiple" : wp.template( 'grunion-field-checkbox-multiple' ),
+			select              : wp.template( 'grunion-field-select' ),
+			date                : wp.template( 'grunion-field-date' ),
+			text                : wp.template( 'grunion-field-text' )
+		},
+		getContent     : function() {
+			var content = this.shortcode.content,
+				index = 0,
+				field, named,
+				body = '';
 
-            // If it's the legacy `[contact-form /]` syntax, populate default fields.
-            if ( ! content ) {
-                content = grunionEditorView.default_form;
-            }
+			// If it's the legacy `[contact-form /]` syntax, populate default fields.
+			if ( ! content ) {
+				content = grunionEditorView.default_form;
+			}
 
-            // Render the fields.
-            while ( field = wp.shortcode.next( 'contact-field', content, index ) ) {
-                index = field.index + field.content.length;
-                named = field.shortcode.attrs.named;
-                if ( ! named.type || ! this.field_templates[ named.type ] ) {
-                    named.type = 'text';
-                }
-                if ( named.required ) {
-                    named.required = grunionEditorView.labels.required_field_text;
-                }
-                if ( named.options && 'string' === typeof named.options ) {
-                    named.options = named.options.split(',');
-                }
-                body += this.field_templates[ named.type ]( named );
-            }
+			// Render the fields.
+			while ( field = wp.shortcode.next( 'contact-field', content, index ) ) {
+				index = field.index + field.content.length;
+				named = field.shortcode.attrs.named;
+				if ( ! named.type || ! this.field_templates[ named.type ] ) {
+					named.type = 'text';
+				}
+				if ( named.required ) {
+					named.required = grunionEditorView.labels.required_field_text;
+				}
+				if ( named.options && 'string' === typeof named.options ) {
+					named.options = named.options.split(',');
+				}
+				body += this.field_templates[ named.type ]( named );
+			}
 
-            options = {
-                body : body,
-                submit_button_text : grunionEditorView.labels.submit_button_text
-            };
+			options = {
+				body : body,
+				submit_button_text : grunionEditorView.labels.submit_button_text
+			};
 
-            return this.template( options );
-        },
-        edit: function( data, update_callback ) {
-            var shortcode_data = wp.shortcode.next( this.shortcode_string, data );
+			return this.template( options );
+		},
+		edit: function( data, update_callback ) {
+			var shortcode_data = wp.shortcode.next( this.shortcode_string, data );
 
-            $modal_wrap = $('#grunion-modal-wrap');
+			$modal_wrap = $('#grunion-modal-wrap');
 
-            open_modal( shortcode_data.shortcode, this, update_callback );
-        }
-    };
-    wp.mce.views.register( 'contact-form', wp.mce.grunion_wp_view_renderer );
+			open_modal( shortcode_data.shortcode, this, update_callback );
+		}
+	};
+	wp.mce.views.register( 'contact-form', wp.mce.grunion_wp_view_renderer );
 
-    open_modal = function( shortcode, renderer, update_callback ) {
-        var $fields = $modal_wrap.find('.fields'),
-            index = 0,
-            named;
+	open_modal = function( shortcode, renderer, update_callback ) {
+		var $fields = $modal_wrap.find('.fields'),
+			index = 0,
+			named;
 
-        $fields.empty();
+		$fields.empty();
 
-        if ( ! shortcode.content ) {
-            shortcode.content = grunionEditorView.default_form;
-        }
+		if ( ! shortcode.content ) {
+			shortcode.content = grunionEditorView.default_form;
+		}
 
-        // Render the fields.
-        while ( field = wp.shortcode.next( 'contact-field', shortcode.content, index ) ) {
-            index = field.index + field.content.length;
-            named = field.shortcode.attrs.named;
-            if ( named.options && 'string' === typeof named.options ) {
-                named.options = named.options.split(',');
-            }
-            $fields.append( renderer.edit_template( named ) );
-        }
+		// Render the fields.
+		while ( field = wp.shortcode.next( 'contact-field', shortcode.content, index ) ) {
+			index = field.index + field.content.length;
+			named = field.shortcode.attrs.named;
+			if ( named.options && 'string' === typeof named.options ) {
+				named.options = named.options.split(',');
+			}
+			$fields.append( renderer.edit_template( named ) );
+		}
 
-        $modal_wrap.show();
-        $modal_wrap.find( '.grunion-modal-backdrop' ).off( 'click', prompt_close ).on( 'click', prompt_close );
-        $modal_wrap.find( '.submit input[name=submit]' ).off( 'click', save_close ).on( 'click', { callback : update_callback }, save_close );
-    };
+		$modal_wrap.show();
+		$modal_wrap.find( '.grunion-modal-backdrop' ).off( 'click', prompt_close ).on( 'click', prompt_close );
+		$modal_wrap.find( '.submit input[name=submit]' ).off( 'click', save_close ).on( 'click', { callback : update_callback }, save_close );
+	};
 
-    save_close = function( event ) {
-        var content = '',
-            attrs = {},
-            shortcode;
+	save_close = function( event ) {
+		var content = '',
+			attrs = {},
+			shortcode;
 
-        $modal_wrap.find('.fields').children().each( function(){
-            var field_shortcode = {
-                    tag   : 'contact-field',
-                    type  : 'single',
-                    attrs : {
-                        label : $(this).find('input[name=label]').val(),
-                        type  : $(this).find('select[name=type]').val(),
-                    }
-                },
-                options = [];
+		$modal_wrap.find('.fields').children().each( function(){
+			var field_shortcode = {
+					tag   : 'contact-field',
+					type  : 'single',
+					attrs : {
+						label : $(this).find('input[name=label]').val(),
+						type  : $(this).find('select[name=type]').val(),
+					}
+				},
+				options = [];
 
-            if ( $(this).find('input[name=required]:checked').length ) {
-                field_shortcode.attrs.required = '1';
-            }
+			if ( $(this).find('input[name=required]:checked').length ) {
+				field_shortcode.attrs.required = '1';
+			}
 
-            $(this).find('input[name=option]').each( function(){
-                if ( $(this).val() ) {
-                    options.push( $(this).val() );
-                }
-            });
-            if ( options.length ) {
-                field_shortcode.attrs.options = options.join(',');
-            }
+			$(this).find('input[name=option]').each( function(){
+				if ( $(this).val() ) {
+					options.push( $(this).val() );
+				}
+			});
+			if ( options.length ) {
+				field_shortcode.attrs.options = options.join(',');
+			}
 
-            content += wp.shortcode.string( field_shortcode );
-        } );
+			content += wp.shortcode.string( field_shortcode );
+		} );
 
-        attrs = {};
+		attrs = {};
 
-        shortcode = {
-            tag     : 'contact-form',
-            type    : 'closed',
-            content : content,
-            attrs   : attrs
-        };
-        event.data.callback( wp.shortcode.string( shortcode ) );
-        $modal_wrap.hide();
-    };
+		shortcode = {
+			tag     : 'contact-form',
+			type    : 'closed',
+			content : content,
+			attrs   : attrs
+		};
+		event.data.callback( wp.shortcode.string( shortcode ) );
+		$modal_wrap.hide();
+	};
 
-    prompt_close = function() {
-        if ( confirm( grunionEditorView.labels.edit_close_ays ) ) {
-            $modal_wrap.hide();
-        }
-    };
+	prompt_close = function() {
+		if ( confirm( grunionEditorView.labels.edit_close_ays ) ) {
+			$modal_wrap.hide();
+		}
+	};
 
 }( jQuery, wp, grunionEditorView ));
