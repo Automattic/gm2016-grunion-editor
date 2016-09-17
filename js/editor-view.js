8846ac032f4555ae579da6ae1bcf6de7774cbfc3
George Stephanis <georgestephanis@automattic.com>
2016-09-17 15:25:53 -0700

    Abstract out the modal functions.

--- js/editor-view.js
+++ js/editor-view.js
@@ -5,6 +5,8 @@
         return;
     }
 
+    var $modal_wrap, open_modal, save_close, prompt_close;
+
     wp.mce.grunion_wp_view_renderer = {
         shortcode_string : 'contact-form',
         shortcode_data : {},
@@ -30,44 +32,7 @@
             var content = this.shortcode.content,
                 index = 0,
                 field, named,
-                body = ''; /* this.field_templates.email({
-                    label : 'Email Address'
-                }) + this.field_templates.telephone({
-                    label : 'Telephone Field'
-                }) + this.field_templates.textarea({
-                    label : 'Textarea Field'
-                }) + this.field_templates.radio({
-                    label : 'Radio Field',
-                    options : [
-                        'Option 1',
-                        'Option 2',
-                        'Option 3'
-                    ],
-                    value : 'Option 2'
-                }) + this.field_templates.checkbox({
-                    label : 'Checkbox Field'
-                }) + this.field_templates['checkbox-multiple']({
-                    label : 'Checkbox Multiple Field',
-                    options : [
-                        'Option 1',
-                        'Option 2',
-                        'Option 3'
-                    ],
-                    value : 'Option 2'
-                }) + this.field_templates.select({
-                    label : 'Select Field',
-                    options : [
-                        'Option 1',
-                        'Option 2',
-                        'Option 3'
-                    ],
-                    value : 'Option 2'
-                }) + this.field_templates.date({
-                    label : 'Date Field'
-                }) + this.field_templates.text({
-                    label : 'Text Field'
-                });
-                */
+                body = '';
 
             // If it's the legacy `[contact-form /]` syntax, populate default fields.
             if ( ! content ) {
@@ -84,7 +49,7 @@
                 if ( named.required ) {
                     named.required = grunionEditorView.labels.required_field_text;
                 }
-                if ( named.options ) {
+                if ( named.options && 'string' === typeof named.options ) {
                     named.options = named.options.split(',');
                 }
                 body += this.field_templates[ named.type ]( named );
@@ -98,65 +63,81 @@
             return this.template( options );
         },
         edit: function( data ) {
-            var $modal_wrap    = $('#grunion-modal-wrap'),
-                shortcode_data = wp.shortcode.next( this.shortcode_string, data ),
-                named          = shortcode_data.shortcode.attrs.named,
-                content        = shortcode_data.shortcode.content,
-                editor         = tinyMCE.activeEditor,
-                renderer       = this,
-                args           = {
-                    tag     : 'contact-form',
-                    type    : 'closed',
-                    content : content,
-                    // Only keep keys we recognize.
-                    attrs   : _.pick( named, _.keys( renderer.defaults ) )
-                },
-                open_modal, save_close, prompt_close;
+            var shortcode_data = wp.shortcode.next( this.shortcode_string, data ),
+                renderer = this;
 
-            open_modal = function( shortcode ) {
-                var $fields = $modal_wrap.find('.fields'),
-                    index = 0,
-                    named;
+            $modal_wrap = $('#grunion-modal-wrap');
 
-                if ( ! shortcode.content ) {
-                    shortcode.content = grunionEditorView.default_form;
-                }
-
-                // Render the fields.
-                while ( field = wp.shortcode.next( 'contact-field', shortcode.content, index ) ) {
-                    index = field.index + field.content.length;
-                    named = field.shortcode.attrs.named;
-                    if ( named.options && 'string' === typeof named.options ) {
-                        console.log( named.options );
-                        named.options = named.options.split(',');
-                    }
-                    $fields.append( renderer.edit_template( named ) );
-                }
-
-                $modal_wrap.show();
-                $modal_wrap.find( '.grunion-modal-backdrop' ).on( 'click', prompt_close );
-                $modal_wrap.find( '.submit input[name=submit]' ).on( 'click', save_close );
-            };
-
-            save_close = function() {
-                editor.insertContent( wp.shortcode.string( args ) );
-                $modal_wrap.hide();
-                $modal_wrap.find( '.grunion-modal-backdrop' ).off( 'click', prompt_close );
-                $modal_wrap.find( '.submit input[name=submit]' ).off( 'click', save_close );
-            };
-
-            prompt_close = function() {
-                if ( confirm( grunionEditorView.labels.edit_close_ays ) ) {
-                    $modal_wrap.hide();
-                    $modal_wrap.find( '.grunion-modal-backdrop' ).off( 'click', prompt_close );
-                    $modal_wrap.find( '.submit input[name=submit]' ).off( 'click', save_close );
-                }
-            };
-
-            open_modal( shortcode_data.shortcode );
+            open_modal( shortcode_data.shortcode, this );
 
         }
     };
     wp.mce.views.register( 'contact-form', wp.mce.grunion_wp_view_renderer );
+
+    open_modal = function( shortcode, renderer ) {
+        var $fields = $modal_wrap.find('.fields'),
+            index = 0,
+            named;
+
+        if ( ! shortcode.content ) {
+            shortcode.content = grunionEditorView.default_form;
+        }
+
+        // Render the fields.
+        while ( field = wp.shortcode.next( 'contact-field', shortcode.content, index ) ) {
+            index = field.index + field.content.length;
+            named = field.shortcode.attrs.named;
+            if ( named.options && 'string' === typeof named.options ) {
+                named.options = named.options.split(',');
+            }
+            $fields.append( renderer.edit_template( named ) );
+        }
+
+        $modal_wrap.show();
+        $modal_wrap.find( '.grunion-modal-backdrop' ).off( 'click', prompt_close ).on( 'click', prompt_close );
+        $modal_wrap.find( '.submit input[name=submit]' ).off( 'click', save_close ).on( 'click', save_close );
+    };
+
+    save_close = function() {
+        var content = '',
+            attrs = {},
+            shortcode;
+
+        $modal_wrap.find('.fields').children().each( function( index ){
+            var field_shortcode = {
+                tag   : 'contact-field',
+                type  : 'single',
+                attrs : {
+                    label : $(this).find('input[name=label]').val(),
+                    type  : $(this).find('select[name=type]').val(),
+                }
+            };
+
+            if ( $(this).find('input[name=required]:checked').length ) {
+                field_shortcode.attrs.required = '1';
+            }
+
+            // DO OPTIONS HERE FOR MULTISELECTS
+
+            content += wp.shortcode.string( field_shortcode );
+        } );
+
+        attrs = {}; // _.pick( named, _.keys( renderer.defaults ) )
+
+        shortcode = {
+            tag     : 'contact-form',
+            type    : 'closed',
+            content : content,
+            attrs   : attrs
+        };
+        tinyMCE.activeEditor.insertContent( wp.shortcode.string( shortcode ) );
+        $modal_wrap.hide();
+    };
+
+    prompt_close = function() {
+        if ( confirm( grunionEditorView.labels.edit_close_ays ) ) {
+            $modal_wrap.hide();
+        }
+    };
 
 }( jQuery, wp, grunionEditorView ));
